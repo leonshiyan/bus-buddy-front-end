a4c996dd826a369e978722563461fb405a7a5267
leonshiyan <leonshiyan@gmail.com>
2023-02-24 20:56:06 -0800

    stop detail update

--- src/pages/MyStopDetails/MyStopDetails.tsx
+++ src/pages/MyStopDetails/MyStopDetails.tsx
@@ -1,34 +1,41 @@
 import { useState,useEffect } from "react"
 import * as apiService from "../../services/apiService"
 import { useParams } from 'react-router-dom'
+//types
+import { SearchResult } from '../../types/models'
 
 type StopParams = {
   stopNo: string
 }
 const MyStopDetails = (): JSX.Element  => {
-  const [stopData, setStopData] = useState<string>();
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([])
   const { stopNo } = useParams<StopParams>()
   useEffect(() => {
     const fetchData = async () => {
       if (stopNo !== undefined) {
-        const response = await apiService.getStop(stopNo)
-        const data = await response.json()
-        setStopData(data)
+        const searchResults = await apiService.getStop(stopNo)
+        setSearchResults(searchResults)
       }
     }
 
     fetchData();
-  }, [stopData]);
+  }, [searchResults]);
 
 
   return (
     <div>
       <h1>Stop Details</h1>
-      {stopData ? (
-        <h1>{stopData}</h1>
-      ) : (
-        <p>Loading...</p>
-      )}
+      {searchResults.map((result) => (
+        <div key={result.RouteNo}>
+          <p>Bus Stop: {stopNo}</p>
+          <p>Bus Route: {result.RouteNo}</p>
+          <p>Route Name: {result.RouteName}</p>
+          <p>Schedules:</p>
+          {result.Schedules.map((schedule) => (
+            <p key={schedule.ExpectedLeaveTime}>Time: {schedule.ExpectedLeaveTime}</p>
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
