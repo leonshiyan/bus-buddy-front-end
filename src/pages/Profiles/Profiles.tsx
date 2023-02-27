ef0698a5c64d6fac48284c4201d0fe746a901299
leonshiyan <leonshiyan@gmail.com>
2023-02-27 13:55:47 -0800

    css update

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,3 +1,5 @@
+// stylesheets
+import styles from './Profiles.module.css'
 // types
 import { Profile } from '../../types/models'
 // components
@@ -13,12 +15,15 @@ const Profiles = (props: ProfilesProps): JSX.Element => {
   if(!profiles.length) return <p>No profiles yet</p>
   
   return (
-    <>
+    <main className={styles.container}>
+
       <h1>Hello. This is a list of all the profiles.</h1>
       {profiles.map((profile: Profile) =>
         <ProfileCard key={profile.id} profile={profile} />
       )}
-    </>
+    
+    </main>
+    
   )
 }
 export default Profiles
